8bdc8d96dde7b630df81e6f1f130c112ec70b399
Joinwoo <inwoo0109@naver.com>
2024-05-30 23:08:53 +0900

    소개 작성

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,75 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <div>
+        <h2>연락처 정보</h2>
+        <ul>
+          <li>
+            <strong>이름:</strong> 조인우
+          </li>
+          <li>
+            <strong>연락처:</strong> +82-10-5561-8770
+          </li>
+          <li>
+            <strong>이메일:</strong> inwoo0109@naver.com
+          </li>
+          <li>
+            <strong>Github:</strong>{' '}
+            <a href="https://github.com/Joinwoo" target="_blank" rel="noreferrer">
+              Joinwoo
+            </a>
+          </li>
+          <li>
+            <strong>LinkedIn:</strong>{' '}
+            <a href="https://www.linkedin.com/in/Joinwoo" target="_blank" rel="noreferrer">
+              Joinwoo
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>보유 기술</h2>
+        <ul>
+          <li>
+            <strong>프로그래밍 언어:</strong> JavaScript, Dart, Java
+          </li>
+          <li>
+            <strong>프레임워크:</strong> React, Flutter
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>주요 이력</h2>
+        <ul>
+          <li>
+            <strong>CNU 정보보호 특화대학 사업단 인턴:</strong> 웹 해킹 학습 및 리눅스 환경에서 웹 워게임 해결 (2024.03~현재)
+          </li>
+          <li>
+            <strong>CNU 겨울 스터디 클럽 학습자:</strong> 알고리즘 스터디 그룹 구성 및 문제 해결 (2023.12~2024.02)
+          </li>
+          <li>
+            <strong>둔산경찰서 앱 개발 프로젝트 디자이너:</strong> Figma를 사용한 UI/UX 디자인 (2023.04~2023.12)
+          </li>
+          <li>
+            <strong>SW 컨버전스 클러스터 2.0 해커톤 프론트엔드 개발자:</strong> React를 사용한 모바일 웹사이트 개발, 대회 우승 (2023.08~2023.09)
+          </li>
+          <li>
+            <strong>제11회 K-해커톤 프론트엔드 개발자:</strong> Flutter를 사용한 모바일 앱 개발, 장려상 수상 (2023.05~2023.06)
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>특이 사항</h2>
+        <p>
+          <strong>문화부장으로서 스타트업 클럽 'Motion'에서 멤버 간의 친목 도모 행사 기획 및 실행 (2024.03~현재)</strong>
+        </p>
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
